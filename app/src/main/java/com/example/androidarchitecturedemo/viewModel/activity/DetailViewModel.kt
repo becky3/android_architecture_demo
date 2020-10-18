2c010b34946624366814565c3cec0bb5a3bcdf7c
becky <>
2020-10-18 10:57:47 +0900

    - お気に入りボタンのダミー実装

--- app/src/main/java/com/example/androidarchitecturedemo/viewModel/activity/DetailViewModel.kt
+++ app/src/main/java/com/example/androidarchitecturedemo/viewModel/activity/DetailViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.androidarchitecturedemo.viewModel.activity
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DetailViewModel: ViewModel() {
 
+    private var _isFavorite = MutableLiveData<Boolean>()
 
+    val isFavorite: LiveData<Boolean> = _isFavorite
+
+    private var __isFavorite: Boolean = false
+
+    fun onFavoriteSelected() {
+        __isFavorite = !__isFavorite
+        _isFavorite.value = __isFavorite
+    }
 
 }
